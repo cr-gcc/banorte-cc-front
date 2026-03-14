2d6d70eb73604c73229c921f8210a6593b11e40c
cris-contact-cinko <cris.gcc@outlook.com>
2026-03-13 18:33:42 -0600

    SideBar Admin

---
+++ src/components/layouts/sidebar/AdminSidebar.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+	<aside class="min-w-64 flex flex-col justify-between bg-gray-200 rounded-lg mt-4 ml-4 mr-4 mb-4">
+		<div class="px-4 py-6">
+      <ul class="space-y-1">
+        <li>
+          <router-link to="/admin/calendario" class="block rounded-lg px-4 py-2 font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-700">
+            <i class="fa-solid fa-calendar mr-2"/>Calendario
+          </router-link>
+        </li>
+      </ul>
+    </div>
+	</aside>
+</template>
