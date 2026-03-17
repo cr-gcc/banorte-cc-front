ca164c1fd2e84d7433459962d62b3f967152d3b6
cris-contact-cinko <cris.gcc@outlook.com>
2026-03-17 17:05:12 -0600

    Componentes atomicos
    Input y button base

---
+++ src/components/atoms/buttons/BaseButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+	defineProps({
+		type: {
+			type: String,
+			default: 'button'
+		},
+		styleClass: {
+			type: String,
+			default: ''
+		}
+	})
+</script>
+<template>
+	<button :type="type" :class="styleClass">
+		<slot />
+	</button>
+</template>
---
+++ src/components/atoms/inputs/BaseInput.vue
@@ -0,0 +1,36 @@
+<script setup>
+	defineProps({
+		id: {
+			type: String,
+			required: true
+		},
+		name: {
+			type: String,
+			required: true
+		},
+		type: {
+			type: String,
+			default: 'text'
+		},
+		modelValue: {
+			type: [String, Number],
+			default: ''
+		},
+		styleClass: {
+			type: String,
+			default: ''
+		}
+	})
+
+	defineEmits(['update:modelValue'])
+</script>
+<template>
+  <input 
+    :id="id" 
+    :name="name"
+		:type="type" 
+    :value="modelValue"
+		:class="styleClass"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
