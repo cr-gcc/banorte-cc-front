efbbefe6597f1496d8c3c9dd00873351bb75238f
cris-contact-cinko <cris.gcc@outlook.com>
2026-03-17 11:24:37 -0600

    Calendario
    Vista base de calendario

--- src/views/admin/CalendarView.vue
+++ src/views/admin/CalendarView.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-    <div>
-        <h1>Calendar View</h1>
-    </div>
+	<div>
+		<div class="mb-2">
+			<h1 class="text-4xl text-red-700 font-semibold">Calendario</h1>
+		</div>
+		<div class="bg-gray-200 rounded-lg">
+			<div class="px-4 py-2">
+				
+			</div>
+		</div>
+	</div>
 </template>
